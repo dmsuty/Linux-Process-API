64546d9adb42524afb3026fed7c4e74fcbfe6852
dmsuty <dmsutyy@gmail.com>
2023-04-22 19:02:35 +0300

    MVP version of API realisation

---
+++ api.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Body
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+from process import PIDProcess, NewProcess
+
+app = FastAPI()
+
+processes_names = (
+    "simple_long_process.py",
+)
+processes = dict()
+for name in processes_names:
+    processes[name] = NewProcess()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/api/docs")
+def docs():
+    return RedirectResponse(url="http://127.0.0.1:8000/docs/")
+
+@app.post("/api/{process_name}")
+def change_status(process_name: str, item: str = Body(...)):
+    if item == "STOP":
+        processes[process_name].kill()
+        return "process was killed"
+    elif item == "START":
+        processes[process_name].run_py(f"processes/{process_name}.py")
+        return "process has been started"
+
+@app.get("/api/{process_name}")
+def get_status(process_name: str):
+    return processes[process_name].get_status()
+
+@app.get("/api/result{process_name}")
+def get_result(process_name: str):
+    return processes[process_name].get_result()
---
+++ processes/simple_long_process.py
@@ -0,0 +1,7 @@
+import time
+
+ans = 0
+while (ans != 30_000_000):
+    ans += 1
+print('Done')
+print('God damn it is not possible')
